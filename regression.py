0b129629151e4ce8543616762d7c87cd27af11cf
Yasemin <ykhanmoradi@gmail.com>
2024-08-22 10:50:47 -0600

    initial commit

---
+++ regression.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# read from csv file 
+data = pd.read_csv('')
